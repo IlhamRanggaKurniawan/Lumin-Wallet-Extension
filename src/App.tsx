fc3827aabf6395e4902bb875ba44689dfca864b4
Ilham Rangga <ilhamranggakurniawan0@gmail.com>
2025-08-23 14:09:57 +0700

    init: initialize the chrome extension set up

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import Avatar from "./components/Avatar"
+import { Button } from "./components/ui/button"
+
+const App = () => {
+
+  return (
+    <div className="w-96 h-screen">
+      <Avatar />
+      <Button>Tes</Button>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Avatar.tsx
@@ -0,0 +1,10 @@
+
+const Avatar = () => {
+  return (
+    <div className='w-12 aspect-square rounded-full overflow-hidden'>
+        <img src='https://github.com/shadcn.png'/>
+    </div>
+  )
+}
+
+export default Avatar
